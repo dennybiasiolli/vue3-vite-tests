fdc3a13995841d8c94c614078bc7d67b291daed4
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-10-26 20:49:39 +0200

    Test1: switching to setup script

--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,3 +1,52 @@
+<script setup>
+import {
+  ref,
+  onMounted,
+  onBeforeUnmount,
+  watch,
+  computed,
+} from 'vue';
+
+const { msg, prop1, prop2 } = defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+  prop1: {
+    type: String,
+    required: true,
+  },
+  prop2: {
+    type: String,
+    required: true,
+  },
+});
+
+const counter = ref(0);
+const isEven = computed(() => counter.value % 2 === 0);
+const interval = ref(null);
+
+const handleStartCounter = () => {
+  interval.value = setInterval(() => {
+    counter.value++;
+  }, 1000);
+};
+const handleStopCounter = () => {
+  clearInterval(interval.value);
+  interval.value = null;
+};
+
+onMounted(handleStartCounter);
+onBeforeUnmount(handleStopCounter);
+
+watch(counter, (newValue, oldValue) => {
+  console.log(`counter changed from ${oldValue} to ${newValue}`);
+});
+watch(msg, (newValue, oldValue) => {
+  console.log(`msg changed from "${oldValue}" to "${newValue}"`);
+});
+</script>
+
 <template>
   <div>
     <h2>{{ msg }}</h2>
@@ -7,71 +56,3 @@
     <button v-else @click="handleStartCounter">Start counter</button>
   </div>
 </template>
-
-<script>
-import {
-  ref,
-  onMounted,
-  onBeforeUnmount,
-  watch,
-  toRefs,
-  toRef,
-  computed,
-} from 'vue';
-
-export default {
-  props: {
-    msg: {
-      type: String,
-      required: true,
-    },
-    prop1: {
-      type: String,
-      required: true,
-    },
-    prop2: {
-      type: String,
-      required: true,
-    },
-  },
-  setup(props) {
-    const { msg, prop1 } = toRefs(props);
-    console.log(prop1);
-    const prop2 = toRef(props, 'prop2');
-    console.log(prop2);
-    const counter = ref(0);
-    const isEven = computed(() => counter.value % 2 === 0);
-    const interval = ref(null);
-
-    const handleStartCounter = () => {
-      interval.value = setInterval(() => {
-        counter.value++;
-      }, 1000);
-    };
-    const handleStopCounter = () => {
-      clearInterval(interval.value);
-      interval.value = null;
-    };
-
-    onMounted(handleStartCounter);
-    onBeforeUnmount(handleStopCounter);
-
-    watch(counter, (newValue, oldValue) => {
-      console.log(`counter changed from ${oldValue} to ${newValue}`);
-    });
-    watch(msg, (newValue, oldValue) => {
-      console.log(`msg changed from "${oldValue}" to "${newValue}"`);
-    });
-
-    // anything returned here will be available for the rest of the component
-    return {
-      msg,
-      counter,
-      isEven,
-      interval,
-      handleStartCounter,
-      handleStopCounter,
-    };
-  },
-};
-</script>
